cd561039c4300ae21151a3d6d6a979299f32cb92
Armel Nzawou <armel.nzawou.developer@gmail.com>
2026-04-08 16:36:30 +0200

    Add Scroll to top Button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+///Hamburger menu Javascript
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
@@ -6,3 +8,29 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
 });
+
+
+
+
+///Scroll to Top button
+
+  const scrollBtn = document.getElementById("scrollTopBtn");
+
+  // Show button after scrolling
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollBtn.classList.remove("hidden");
+      scrollBtn.classList.add("flex");
+    } else {
+      scrollBtn.classList.add("hidden");
+      scrollBtn.classList.remove("flex");
+    }
+  });
+
+  // Smooth scroll to top
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
